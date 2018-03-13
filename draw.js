8d81fd31350166b2ae9abd9beae117bcc603977e
Tiffany Chen <tiffanysta.c@gmail.com>
2018-03-13 14:05:23 -0400

    pushed

---
+++ draw.js
@@ -0,0 +1,16 @@
+var germany = [14, 10, 7];
+var oar = [2, 6, 9];
+
+var bgermany = document.getElementById("germany");
+var boar = document.getElementById("oar");
+
+var c1 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+c1.setAttribute("cx", this.cx);
+c1.setAttribute("cy", this.cy);
+c1.setAttribute("r", 10);
+c1.setAttribute("stroke", "#FFFFFF");
+c1.setAttribute("fill", this.fill);
+svg.appendChild(c1);
+
+bgermany.addEventListener('click', );
+boar.addEventListener('click', );
